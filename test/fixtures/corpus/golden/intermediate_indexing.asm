1e3f2b86777d02043fbf16c44bea7d9618ee7373
John Hardy <jh@lagado.com>
2026-02-15 16:34:19 +1100

    feat(lowering): render fixup-backed asm as canonical instructions

--- test/fixtures/corpus/golden/intermediate_indexing.asm
+++ test/fixtures/corpus/golden/intermediate_indexing.asm
@@ -3,30 +3,30 @@
 
 ; func main begin
 main:
-db $3A, lo(idx), hi(idx)                           ; 0000: 3A 00 00
+ld A, (idx)                                        ; 0000: 3A 00 00
 ld H, $0000                                        ; 0003: 26 00
 ld L, A                                            ; 0005: 6F
 push HL                                            ; 0006: E5
 pop HL                                             ; 0007: E1
 push HL                                            ; 0008: E5
-db $21, lo(arr), hi(arr)                           ; 0009: 21 00 00
+ld HL, arr                                         ; 0009: 21 00 00
 pop DE                                             ; 000C: D1
 add HL, DE                                         ; 000D: 19
 push HL                                            ; 000E: E5
 pop HL                                             ; 000F: E1
 ld r, (hl)                                         ; 0010: 7E
-db $2A, lo(idxw), hi(idxw)                         ; 0011: 2A 00 00
+ld HL, (idxw)                                      ; 0011: 2A 00 00
 push HL                                            ; 0014: E5
 pop HL                                             ; 0015: E1
 push HL                                            ; 0016: E5
-db $21, lo(arr), hi(arr)                           ; 0017: 21 00 00
+ld HL, arr                                         ; 0017: 21 00 00
 pop DE                                             ; 001A: D1
 add HL, DE                                         ; 001B: 19
 push HL                                            ; 001C: E5
 pop HL                                             ; 001D: E1
 ld r, (hl)                                         ; 001E: 7E
 push HL                                            ; 001F: E5
-db $21, lo(arr), hi(arr)                           ; 0020: 21 00 00
+ld HL, arr                                         ; 0020: 21 00 00
 pop DE                                             ; 0023: D1
 add HL, DE                                         ; 0024: 19
 push HL                                            ; 0025: E5
@@ -36,7 +36,7 @@ ld a, (hl)                                         ; 0028: 7E
 ld L, A                                            ; 0029: 6F
 ld H, $0000                                        ; 002A: 26 00
 push HL                                            ; 002C: E5
-db $21, lo(arr), hi(arr)                           ; 002D: 21 00 00
+ld HL, arr                                         ; 002D: 21 00 00
 pop DE                                             ; 0030: D1
 add HL, DE                                         ; 0031: 19
 push HL                                            ; 0032: E5
@@ -44,7 +44,7 @@ pop HL                                             ; 0033: E1
 ld r, (hl)                                         ; 0034: 7E
 ld HL, $0000                                       ; 0035: 21 00 00
 push HL                                            ; 0038: E5
-db $3A, lo(idx), hi(idx)                           ; 0039: 3A 00 00
+ld A, (idx)                                        ; 0039: 3A 00 00
 ld H, $0000                                        ; 003C: 26 00
 ld L, A                                            ; 003E: 6F
 push HL                                            ; 003F: E5
@@ -52,7 +52,7 @@ pop HL                                             ; 0040: E1
 add HL, HL                                         ; 0041: 29
 add HL, HL                                         ; 0042: 29
 push HL                                            ; 0043: E5
-db $21, lo(grid), hi(grid)                         ; 0044: 21 00 00
+ld HL, grid                                        ; 0044: 21 00 00
 pop DE                                             ; 0047: D1
 add HL, DE                                         ; 0048: 19
 push HL                                            ; 0049: E5
