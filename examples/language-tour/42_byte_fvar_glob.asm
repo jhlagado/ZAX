a5601c22664fc469ad70df68ae26394ce5abef32
John Hardy <jh@lagado.com>
2026-03-07 09:27:36 +1100

    Add byte index imm pipeline support

--- examples/language-tour/42_byte_fvar_glob.asm
+++ examples/language-tour/42_byte_fvar_glob.asm
@@ -9,77 +9,63 @@ add IX, SP                     ; 0106: DD 39
 push AF                        ; 0108: F5
 push BC                        ; 0109: C5
 push DE                        ; 010A: D5
-ld hl, (glob_idx_word)         ; 010B: 2A 00 00
-push HL                        ; 010E: E5
-pop HL                         ; 010F: E1
-push HL                        ; 0110: E5
-push DE                        ; 0111: D5
-push IX                        ; 0112: DD E5
-pop HL                         ; 0114: E1
-ld DE, $0004                   ; 0115: 11 04 00
-add HL, DE                     ; 0118: 19
+push DE                        ; 010B: D5
+push HL                        ; 010C: E5
+ld E, (IX + $0004)             ; 010D: DD 5E 04
+ld D, (IX + $0005)             ; 0110: DD 56 05
+ld hl, (glob_idx_word)         ; 0113: 2A 00 00
+add HL, DE                     ; 0116: 19
+ld A, (HL)                     ; 0117: 7E
+pop HL                         ; 0118: E1
 pop DE                         ; 0119: D1
-pop DE                         ; 011A: D1
-add HL, DE                     ; 011B: 19
-push HL                        ; 011C: E5
-pop HL                         ; 011D: E1
-ld A, (hl)                     ; 011E: 7E
-push AF                        ; 011F: F5
-ld hl, (glob_idx_word)         ; 0120: 2A 00 00
-push HL                        ; 0123: E5
-pop HL                         ; 0124: E1
-push HL                        ; 0125: E5
-push DE                        ; 0126: D5
-push IX                        ; 0127: DD E5
-pop HL                         ; 0129: E1
-ld DE, $0004                   ; 012A: 11 04 00
-add HL, DE                     ; 012D: 19
-pop DE                         ; 012E: D1
-pop DE                         ; 012F: D1
-add HL, DE                     ; 0130: 19
-push HL                        ; 0131: E5
-pop HL                         ; 0132: E1
-ld (hl), A                     ; 0133: 77
-pop AF                         ; 0134: F1
-ld H, $0000                    ; 0135: 26 00
-ld L, A                        ; 0137: 6F
+push DE                        ; 011A: D5
+push HL                        ; 011B: E5
+ld E, (IX + $0004)             ; 011C: DD 5E 04
+ld D, (IX + $0005)             ; 011F: DD 56 05
+ld hl, (glob_idx_word)         ; 0122: 2A 00 00
+add HL, DE                     ; 0125: 19
+ld (HL), A                     ; 0126: 77
+pop HL                         ; 0127: E1
+pop DE                         ; 0128: D1
+ld H, $0000                    ; 0129: 26 00
+ld L, A                        ; 012B: 6F
 __zax_epilogue_0:
-pop DE                         ; 0138: D1
-pop BC                         ; 0139: C1
-pop AF                         ; 013A: F1
-ld SP, IX                      ; 013B: DD F9
-pop IX                         ; 013D: DD E1
-ret                            ; 013F: C9
+pop DE                         ; 012C: D1
+pop BC                         ; 012D: C1
+pop AF                         ; 012E: F1
+ld SP, IX                      ; 012F: DD F9
+pop IX                         ; 0131: DD E1
+ret                            ; 0133: C9
 ; func byte_fvar_glob end
 ; func main begin
 main:
-push IX                        ; 0140: DD E5
-ld IX, $0000                   ; 0142: DD 21 00 00
-add IX, SP                     ; 0146: DD 39
-push AF                        ; 0148: F5
-push BC                        ; 0149: C5
-push DE                        ; 014A: D5
-push HL                        ; 014B: E5
-ld HL, glob_bytes              ; 014C: 21 00 00
-push HL                        ; 014F: E5
-call byte_fvar_glob            ; 0150: CD 00 00
-inc SP                         ; 0153: 33
-inc SP                         ; 0154: 33
+push IX                        ; 0134: DD E5
+ld IX, $0000                   ; 0136: DD 21 00 00
+add IX, SP                     ; 013A: DD 39
+push AF                        ; 013C: F5
+push BC                        ; 013D: C5
+push DE                        ; 013E: D5
+push HL                        ; 013F: E5
+ld HL, glob_bytes              ; 0140: 21 00 00
+push HL                        ; 0143: E5
+call byte_fvar_glob            ; 0144: CD 00 00
+inc SP                         ; 0147: 33
+inc SP                         ; 0148: 33
 __zax_epilogue_1:
-pop HL                         ; 0155: E1
-pop DE                         ; 0156: D1
-pop BC                         ; 0157: C1
-pop AF                         ; 0158: F1
-ld SP, IX                      ; 0159: DD F9
-pop IX                         ; 015B: DD E1
-ret                            ; 015D: C9
+pop HL                         ; 0149: E1
+pop DE                         ; 014A: D1
+pop BC                         ; 014B: C1
+pop AF                         ; 014C: F1
+ld SP, IX                      ; 014D: DD F9
+pop IX                         ; 014F: DD E1
+ret                            ; 0151: C9
 ; func main end
 
 ; symbols:
 ; label byte_fvar_glob = $0100
-; label __zax_epilogue_0 = $0138
-; label main = $0140
-; label __zax_epilogue_1 = $0155
+; label __zax_epilogue_0 = $012C
+; label main = $0134
+; label __zax_epilogue_1 = $0149
 ; data glob_bytes = $2000
 ; data glob_idx_word = $2008
 ; label __zax_startup = $200A
