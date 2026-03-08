3514d78fb7ec1ee570bf253d6bfe6723ba9c9663
John Hardy <jh@lagado.com>
2026-03-08 22:27:10 +1100

    Add diagnostics for unsupported typed-EA HL stores

--- examples/language-tour/69_word_glob_glob.asm
+++ examples/language-tour/69_word_glob_glob.asm
@@ -21,49 +21,61 @@ ld L, E                        ; 0117: 6B
 ld H, D                        ; 0118: 62
 pop DE                         ; 0119: D1
 push DE                        ; 011A: D5
-push HL                        ; 011B: E5
-ld de, glob_words              ; 011C: 11 00 00
+ex DE, HL                      ; 011B: EB
+push AF                        ; 011C: F5
+push BC                        ; 011D: C5
+push DE                        ; 011E: D5
 ld hl, (glob_idx_word)         ; 011F: 2A 00 00
-add HL, HL                     ; 0122: 29
-add HL, DE                     ; 0123: 19
-pop DE                         ; 0124: D1
-ld (HL), E                     ; 0125: 73
-inc HL                         ; 0126: 23
-ld (HL), D                     ; 0127: 72
-pop DE                         ; 0128: D1
-__zax_epilogue_0:
+push HL                        ; 0122: E5
+pop HL                         ; 0123: E1
+add HL, HL                     ; 0124: 29
+push HL                        ; 0125: E5
+ld HL, glob_words              ; 0126: 21 00 00
 pop DE                         ; 0129: D1
-pop BC                         ; 012A: C1
-pop AF                         ; 012B: F1
-ld SP, IX                      ; 012C: DD F9
-pop IX                         ; 012E: DD E1
-ret                            ; 0130: C9
+add HL, DE                     ; 012A: 19
+push HL                        ; 012B: E5
+pop HL                         ; 012C: E1
+pop DE                         ; 012D: D1
+pop BC                         ; 012E: C1
+pop AF                         ; 012F: F1
+ld (hl), E                     ; 0130: 73
+inc HL                         ; 0131: 23
+ld (hl), D                     ; 0132: 72
+ex DE, HL                      ; 0133: EB
+pop DE                         ; 0134: D1
+__zax_epilogue_0:
+pop DE                         ; 0135: D1
+pop BC                         ; 0136: C1
+pop AF                         ; 0137: F1
+ld SP, IX                      ; 0138: DD F9
+pop IX                         ; 013A: DD E1
+ret                            ; 013C: C9
 ; func main begin
 ; func word_glob_glob end
 main:
-push IX                        ; 0131: DD E5
-ld IX, $0000                   ; 0133: DD 21 00 00
-add IX, SP                     ; 0137: DD 39
-push AF                        ; 0139: F5
-push BC                        ; 013A: C5
-push DE                        ; 013B: D5
-push HL                        ; 013C: E5
-call word_glob_glob            ; 013D: CD 00 00
+push IX                        ; 013D: DD E5
+ld IX, $0000                   ; 013F: DD 21 00 00
+add IX, SP                     ; 0143: DD 39
+push AF                        ; 0145: F5
+push BC                        ; 0146: C5
+push DE                        ; 0147: D5
+push HL                        ; 0148: E5
+call word_glob_glob            ; 0149: CD 00 00
 __zax_epilogue_1:
-pop HL                         ; 0140: E1
-pop DE                         ; 0141: D1
-pop BC                         ; 0142: C1
-pop AF                         ; 0143: F1
-ld SP, IX                      ; 0144: DD F9
-pop IX                         ; 0146: DD E1
-ret                            ; 0148: C9
+pop HL                         ; 014C: E1
+pop DE                         ; 014D: D1
+pop BC                         ; 014E: C1
+pop AF                         ; 014F: F1
+ld SP, IX                      ; 0150: DD F9
+pop IX                         ; 0152: DD E1
+ret                            ; 0154: C9
 ; func main end
 
 ; symbols:
 ; label word_glob_glob = $0100
-; label __zax_epilogue_0 = $0129
-; label main = $0131
-; label __zax_epilogue_1 = $0140
+; label __zax_epilogue_0 = $0135
+; label main = $013D
+; label __zax_epilogue_1 = $014C
 ; data glob_words = $2000
 ; data glob_idx_word = $2010
 ; label __zax_startup = $2012
